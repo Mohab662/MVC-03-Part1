8a3b46957a1a54751aa25d609d2e61647bd73a23
Mohab662 <mohabmbelkan@gmail.com>
2024-09-02 18:56:45 +0300

    Add project files.

---
+++ MVC03/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC03.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Dafault Action
+        // GetAll => Index 
+        public IActionResult Index()
+        {
+            //ViewResult viewResult = new ViewResult();
+            //return viewResult;
+
+            return View(); // Return view with the same name of action (Index)
+        }
+        public IActionResult AboutUs()
+        {
+            return View(); 
+        }
+        public IActionResult Privcy()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
